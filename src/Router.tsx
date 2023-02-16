bf91832379cd28b4a50f17d7d9fe52b89fc64d72
Zacharie Boisnard <mecazack@gmail.com>
2023-02-16 11:04:47 +0100

    feat: routing

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import PageNotFound from './pages/404';
+import APropos from './pages/A_Propos';
+import FicheLogement from './pages/Fiche_Logement';
 import Home from './pages/Home';
 import Layout from './pages/Layout';
 
@@ -10,6 +12,8 @@ const Rooter: React.FC = () => {
             <Routes>
                 <Route path="/" element={<Layout />}>
                     <Route path="/" element={<Home />} />
+                    <Route path="/A_Propos" element={<APropos />} />
+                    <Route path="/Fiche_Logement" element={<FicheLogement />} />
                     <Route path="*" element={<PageNotFound />} />
                 </Route>
             </Routes>
