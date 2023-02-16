aca4100d836167232928ab8746b4f40410a2c838
Zacharie Boisnard <mecazack@gmail.com>
2023-02-16 14:09:49 +0100

    feat: header et footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import LogoApp from '../LogoApp';
 
 const Footer: React.FC = () => {
-    return <footer></footer>;
+    return (
+        <footer className="footer-app">
+            <LogoApp color="#ffffff" variant="footer" />
+            <p className="footer-app__text">© 2020 Kasa. All rights reserved</p>
+        </footer>
+    );
 };
 
 export default Footer;
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,53 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
+import LogoApp from '../LogoApp';
 
 const Header: React.FC = () => {
-    return <header className="header-app"></header>;
+    return (
+        <header className="header-app">
+            <LogoApp variant="header" />
+            <nav className="header-app__nav">
+                <ul className="header-app__nav__liste">
+                    <li className="header-app__nav__liste__item">
+                        <NavLink
+                            to="/"
+                            className={({ isActive }) =>
+                                [
+                                    'header-app__nav__liste__item__link',
+                                    isActive
+                                        ? 'header-app__nav__liste__item__link--active'
+                                        : null,
+                                ]
+                                    .filter(Boolean)
+                                    .join(' ')
+                            }
+                            end
+                        >
+                            Accueil
+                        </NavLink>
+                    </li>
+                    <li className="header-app__nav__liste__item">
+                        <NavLink
+                            to="/A_Propos"
+                            className={({ isActive }) =>
+                                [
+                                    'header-app__nav__liste__item__link',
+                                    isActive
+                                        ? 'header-app__nav__liste__item__link--active'
+                                        : null,
+                                ]
+                                    .filter(Boolean)
+                                    .join(' ')
+                            }
+                            end
+                        >
+                            A Propos
+                        </NavLink>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    );
 };
 
 export default Header;
